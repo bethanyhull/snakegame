28f8e068b8e7883c9d2712bd50936e9e5626896e
Bethany <bethany.hull@gmail.com>
2019-02-22 18:43:48 -0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -3,3 +3,11 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
+
+const x = Math.floor(Math.random() * window.innerWidth);
+const y = Math.floor(Math.random() * window.innerHeight);
+
+c.beginPath();
+c.arc(x, y, 30, 0, Math.PI * 2, true);
+c.fillStyle = `#ffffff`;
+c.fill();
