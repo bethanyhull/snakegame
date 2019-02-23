c35f1c00d43d82b55693338bbe6a9cf60161a80a
Bethany <bethany.hull@gmail.com>
2019-02-22 18:45:39 -0700

    we have dot!

--- canvas.js
+++ canvas.js
@@ -8,6 +8,6 @@ const x = Math.floor(Math.random() * window.innerWidth);
 const y = Math.floor(Math.random() * window.innerHeight);
 
 c.beginPath();
-c.arc(x, y, 30, 0, Math.PI * 2, true);
+c.arc(x, y, 10, 0, Math.PI * 2, true);
 c.fillStyle = `#ffffff`;
 c.fill();
