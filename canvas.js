6b459b96300cbbaf2ee2329b3d49f8783b21639d
Bethany <bethany.hull@gmail.com>
2019-02-22 19:17:34 -0700

    Added keypress function for future code

--- canvas.js
+++ canvas.js
@@ -1,3 +1,28 @@
+// detect key presses- put code in if statements
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+    }
+
+}
+
+// keypress fuction code end
+
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
