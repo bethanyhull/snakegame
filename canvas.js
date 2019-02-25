2861785355c13e7349bd8b0fa31ec09cb6611d84
Bethany <bethany.hull@gmail.com>
2019-02-25 14:47:20 -0700

    messing arounds

--- canvas.js
+++ canvas.js
@@ -1,38 +1,61 @@
-// detect key presses- put code in if statements
-
-document.onkeydown = checkKey;
-
-function checkKey(e) {
-
-    e = e || window.event;
-
-    if (e.key == '38') {
-        // up arrow
-    }
-    else if (e.key == '40') {
-        // down arrow
-    }
-    else if (e.key == '37') {
-       // left arrow
-    }
-    else if (e.key == '39') {
-       // right arrow
-    }
-
-}
-
-// keypress fuction code end
-
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-const x = Math.floor(Math.random() * window.innerWidth);
-const y = Math.floor(Math.random() * window.innerHeight);
+//const x = Math.floor(Math.random() * window.innerWidth);
+//const y = Math.floor(Math.random() * window.innerHeight);
 
-c.beginPath();
-c.arc(x, y, 10, 0, Math.PI * 2, true);
-c.fillStyle = `#ffffff`;
-c.fill();
+// detect key presses- put code in if statements
+
+document.onkeydown = checkKey;
+
+var snakeX = 200
+var snakeY = 200
+dy = 10;
+dx = 10
+
+let snake = [{x: 150, y: 150},
+             {x: 140, y: 140},
+             {x: 130, y: 140},
+             {x: 120, y: 140}]
+
+
+function checkKey(e) {
+
+    e = e || window.event;
+    if (e.key == '38') {
+      dy = 10;
+        // up arrow
+    }
+    else if (e.key == '40') {
+      dy = -10;
+        // down arrow
+    }
+    else if (e.key == '37') {
+      dx = -10;
+       // left arrow
+    }
+    else if (e.key == '39') {
+      dx = 10;
+       // right arrow
+    }
+
+}
+
+function drawSnake(snakePart) {
+  c.beginPath();
+  c.arc(snakePart.x, snakePart.y, 10, 0, Math.PI * 2, true);
+  c.fillStyle = `#ffffff`;
+  c.fill();
+};
+
+drawSnake(snake);
+
+function drawBigSnake() {
+  snake.forEach(drawSnake());
+
+};
+
+drawBigSnake();
